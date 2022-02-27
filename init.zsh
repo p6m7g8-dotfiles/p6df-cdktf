d72afddb63a1fb2a4200c2c4e225665a78a0ea52
Philip M. Gollucci <pgollucci@p6m7g8.com>
2022-02-27 17:43:10 -0500

    feat(clones): adds clones (#24)

--- init.zsh
+++ init.zsh
@@ -6,7 +6,7 @@
 #
 #>
 ######################################################################
-p6df::modules::cfktf::deps() {
+p6df::modules::cdktf::deps() {
   ModuleDeps=(
     p6m7g8/p6df-awscdk
     p6m7g8/p6df-terraform
@@ -21,7 +21,13 @@ p6df::modules::cfktf::deps() {
 #
 #>
 ######################################################################
-p6df::modules::cfktf::langs() {
+p6df::modules::cdktf::langs() {
 
   npm install -g cdktf-cli@next
 }
+
+p6df::modules::cdktf::clones() {
+
+  p6_run_parallel "0" "4" "$(cat $P6_DFZ_SRC_P6M7G8_DIR/p6df-cdktf/conf/cdktf)" "p6_github_util_repo_clone_or_pull_no_ou" "$P6_DFZ_SRC_FOCUSED_DIR"
+}
+
